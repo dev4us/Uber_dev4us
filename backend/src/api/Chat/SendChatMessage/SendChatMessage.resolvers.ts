9f35676e0e9b0b0bf14b90e58a9b22d6d919a4ad
dev4us <bronxofsoul@gmail.com>
2019-01-18 23:02:03 +0900

    End of backend, create frontend Dir

---
+++ backend/src/api/Chat/SendChatMessage/SendChatMessage.resolvers.ts
@@ -0,0 +1,64 @@
+import { Resolvers } from "../../../../src/types/resolvers";
+import privateResolver from "../../../../src/utils/resolverMiddleware";
+import {
+  SendChatMessageMutationArgs,
+  SendChatMessageResponse
+} from "../../../../src/types/graphql";
+import User from "../../../../src/entities/User";
+import Message from "../../../../src/entities/Message";
+import Chat from "../../../../src/entities/Chat";
+
+const resolvers: Resolvers = {
+  Mutation: {
+    SendChatMessage: privateResolver(
+      async (
+        _,
+        args: SendChatMessageMutationArgs,
+        { req, pubSub }
+      ): Promise<SendChatMessageResponse> => {
+        const user: User = req.user;
+        try {
+          const chat = await Chat.findOne({ id: args.chatId });
+
+          if (chat) {
+            if (chat.passengerId === user.id || chat.driverId === user.id) {
+              const message = await Message.create({
+                text: args.text,
+                chat,
+                user
+              }).save();
+              pubSub.publish("newChatMessage", {
+                MessageSubscription: message
+              });
+              return {
+                ok: true,
+                error: null,
+                message
+              };
+            } else {
+              return {
+                ok: false,
+                error: "Unauthorized",
+                message: null
+              };
+            }
+          } else {
+            return {
+              ok: false,
+              error: "Chat not found",
+              message: null
+            };
+          }
+        } catch (error) {
+          return {
+            ok: false,
+            error: error.message,
+            message: null
+          };
+        }
+      }
+    )
+  }
+};
+
+export default resolvers;
