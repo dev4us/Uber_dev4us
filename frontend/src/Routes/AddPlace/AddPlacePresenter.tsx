16a91704277fb414cdd58ee7504aafbdc6b3186f
dev4us <bronxofsoul@gmail.com>
2019-02-03 00:14:53 +0900

    dev AddPlaces

---
+++ frontend/src/Routes/AddPlace/AddPlacePresenter.tsx
@@ -0,0 +1,68 @@
+import React from "react";
+import { MutationFn } from "react-apollo";
+import Helmet from "react-helmet";
+import { Link } from "react-router-dom";
+import Button from "../../Components/Button";
+import Form from "../../Components/Form";
+import Header from "../../Components/Header";
+import Input from "../../Components/Input";
+import styled from "../../typed-components";
+
+const Container = styled.div`
+  padding: 0 40px;
+`;
+
+const ExtendedInput = styled(Input)`
+  margin-bottom: 40px;
+`;
+
+const ExtendedLink = styled(Link)`
+  text-decoration: underline;
+  margin-bottom: 20px;
+  display: block;
+`;
+
+interface IProps {
+  address: string;
+  name: string;
+  onInputChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  loading: boolean;
+  onSubmit: MutationFn;
+}
+
+const AddPlacePresenter: React.SFC<IProps> = ({
+  onInputChange,
+  address,
+  name,
+  loading,
+  onSubmit
+}) => (
+  <React.Fragment>
+    <Helmet>
+      <title>Add Place | Nuber</title>
+    </Helmet>
+    <Header title={"Add Place"} backTo={"/"} />
+    <Container>
+      <Form submitFn={onSubmit}>
+        <ExtendedInput
+          placeholder={"Name"}
+          type={"text"}
+          onChange={onInputChange}
+          value={name}
+          name={"name"}
+        />
+        <ExtendedInput
+          placeholder={"Address"}
+          type={"text"}
+          onChange={onInputChange}
+          value={address}
+          name={"address"}
+        />
+        <ExtendedLink to={"/find-address"}>Pick place from map</ExtendedLink>
+        <Button onClick={null} value={loading ? "Adding place" : "Add Place"} />
+      </Form>
+    </Container>
+  </React.Fragment>
+);
+
+export default AddPlacePresenter;
