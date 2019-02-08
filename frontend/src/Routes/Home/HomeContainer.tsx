41c68cf394b7af9b6fbaa24e45d02a5f108f2061
dev4us <bronxofsoul@gmail.com>
2019-02-08 21:58:09 +0900

    dev RouteToRoute Render

--- frontend/src/Routes/Home/HomeContainer.tsx
+++ frontend/src/Routes/Home/HomeContainer.tsx
@@ -1,15 +1,23 @@
 import React from "react";
 import { Query } from "react-apollo";
 import ReactDOM from "react-dom";
-import { RouteComponentProps } from "react-router";
+import { RouteComponentProps } from "react-router-dom";
+import { toast } from "react-toastify";
+import { geoCode } from "../../mapHelpers";
 import { USER_PROFILE } from "../../sharedQueries";
 import { userProfile } from "../../types/api";
 import HomePresenter from "./HomePresenter";
 
 interface IState {
   isMenuOpen: boolean;
+  toAddress: string;
+  toLat: number;
+  toLng: number;
   lat: number;
   lng: number;
+  distance?: string;
+  duration?: string;
+  price?: number;
 }
 
 interface IProps extends RouteComponentProps<any> {
@@ -22,10 +30,15 @@ class HomeContainer extends React.Component<IProps, IState> {
   public mapRef: any;
   public map: google.maps.Map;
   public userMarker: google.maps.Marker;
+  public toMarker: google.maps.Marker;
+  public directions: google.maps.DirectionsRenderer;
   public state = {
     isMenuOpen: false,
     lat: 0,
-    lng: 0
+    lng: 0,
+    toAddress: "",
+    toLat: 0,
+    toLng: 0
   };
   constructor(props) {
     super(props);
@@ -38,7 +51,7 @@ class HomeContainer extends React.Component<IProps, IState> {
     );
   }
   public render() {
-    const { isMenuOpen } = this.state;
+    const { isMenuOpen, toAddress } = this.state;
     return (
       <ProfileQuery query={USER_PROFILE}>
         {({ loading }) => (
@@ -47,6 +60,9 @@ class HomeContainer extends React.Component<IProps, IState> {
             isMenuOpen={isMenuOpen}
             toggleMenu={this.toggleMenu}
             mapRef={this.mapRef}
+            toAddress={toAddress}
+            onInputChange={this.onInputChange}
+            onAddressSubmit={this.onAddressSubmit}
           />
         )}
       </ProfileQuery>
@@ -70,7 +86,6 @@ class HomeContainer extends React.Component<IProps, IState> {
     this.loadMap(latitude, longitude);
   };
   public loadMap = (lat, lng) => {
-    console.log(lat, lng);
     const { google } = this.props;
     const maps = google.maps;
     const mapNode = ReactDOM.findDOMNode(this.mapRef.current);
@@ -80,8 +95,7 @@ class HomeContainer extends React.Component<IProps, IState> {
         lng
       },
       disableDefaultUI: true,
-      minZoom: 8,
-      zoom: 15
+      zoom: 13
     };
     this.map = new maps.Map(mapNode, mapConfig);
     const userMarkerOptions: google.maps.MarkerOptions = {
@@ -117,6 +131,84 @@ class HomeContainer extends React.Component<IProps, IState> {
   };
   public handleGeoError = () => {
     console.log("No location");
+  };
+  public onInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const {
+      target: { name, value }
+    } = event;
+    this.setState({
+      [name]: value
+    } as any);
+  };
+  public onAddressSubmit = async () => {
+    const { toAddress } = this.state;
+    const { google } = this.props;
+    const maps = google.maps;
+    const result = await geoCode(toAddress);
+    if (result !== false) {
+      const { lat, lng, formatted_address: formatedAddress } = result;
+      if (this.toMarker) {
+        this.toMarker.setMap(null);
+      }
+      const toMarkerOptions: google.maps.MarkerOptions = {
+        position: {
+          lat,
+          lng
+        }
+      };
+      this.toMarker = new maps.Marker(toMarkerOptions);
+      this.toMarker.setMap(this.map);
+      const bounds = new maps.LatLngBounds();
+      bounds.extend({ lat, lng });
+      bounds.extend({ lat: this.state.lat, lng: this.state.lng });
+      this.map.fitBounds(bounds);
+      this.setState(
+        {
+          toAddress: formatedAddress,
+          toLat: lat,
+          toLng: lng
+        },
+        this.createPath
+      );
+    }
+  };
+  public createPath = () => {
+    const { toLat, toLng, lat, lng } = this.state;
+    if (this.directions) {
+      this.directions.setMap(null);
+    }
+    const renderOptions: google.maps.DirectionsRendererOptions = {
+      polylineOptions: {
+        strokeColor: "#000"
+      },
+      suppressMarkers: true
+    };
+    this.directions = new google.maps.DirectionsRenderer(renderOptions);
+    const directionsService: google.maps.DirectionsService = new google.maps.DirectionsService();
+    const to = new google.maps.LatLng(toLat, toLng);
+    const from = new google.maps.LatLng(lat, lng);
+    const directionsOptions: google.maps.DirectionsRequest = {
+      destination: to,
+      origin: from,
+      travelMode: google.maps.TravelMode.DRIVING
+    };
+    directionsService.route(directionsOptions, (result, status) => {
+      if (status === google.maps.DirectionsStatus.OK) {
+        const { routes } = result;
+        const {
+          distance: { text: distance },
+          duration: { text: duration }
+        } = routes[0].legs[0];
+        this.setState({
+          distance,
+          duration
+        });
+        this.directions.setDirections(result);
+        this.directions.setMap(this.map);
+      } else {
+        toast.error("There is no route there, you have to ");
+      }
+    });
   };
 }
 
